588560cd831cac6846824a8e9201c5993b9afc1f
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-17 18:41:15 +0100

    Temp

---
+++ mocha.js
@@ -0,0 +1,29 @@
+var Mocha = require('mocha'),
+  fs = require('fs'),
+  Glob = require('glob'),
+  path = require('path')
+
+// Instantiate a Mocha instance.
+var mocha = new Mocha({
+    ui: 'tdd',
+    reporter: 'mock-local-storage'
+})
+
+const globPatterns = {
+  unit: 'src/**/*.spec.js',
+}
+
+let pattern = Object.keys(globPatterns).map((n) => globPatterns[n])
+
+Glob(
+  pattern.length > 1 ? `{${pattern.join(',')}}` : pattern[0],
+  {},
+  (err, files) => {
+    files.forEach((file) => mocha.addFile(file))
+    mocha.run((failures) => {
+      process.on('exit', () => {
+        process.exit(failures)
+      })
+    })
+  }
+)
