4d0a891db5aa9913fe9262646a264215dfd5753a
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-22 23:35:25 +0100

    Unit test for total function

--- scripts/total-unit-prices.js
+++ scripts/total-unit-prices.js
@@ -1,12 +1,8 @@
-import chalk from 'chalk'
-import jsonfile from 'jsonfile'
-
-const log = console.log
-
 function totalUnitPrices(collection) {
-  return collection.reduce(function(previousValue, currentValue, currentIndex, array) {
-    return previousValue + currentValue.unit_price;
-  })
+  let returnTotal = collection.reduce((total, current) =>
+    parseFloat(total) + parseFloat(current.unit_price)
+  , 0)
+  return returnTotal.toFixed(2)
 }
 
 export default totalUnitPrices
--- scripts/total-unit-prices.unit.js
+++ scripts/total-unit-prices.unit.js
@@ -1,18 +1,9 @@
 import totalUnitPrices from './total-unit-prices'
-import faker from 'faker'
+import stub from './stub.json'
 import { expect } from 'chai'
 
 describe('totalUnitPrices', () => {
-  let makeProduct = () => {
-    return {
-      unit_price: faker.commerce.price(1, 500, 2),
-    }
-  }
-  const props = {
-    collection: [makeProduct(), makeProduct(), makeProduct()]
-  }
-  const wrapper =
   it('it gets the right price', () => {
-    expect(totalUnitPrices({...props})).to.equal(55)
+    expect(totalUnitPrices(stub)).to.equal("15.10")
   })
 })
