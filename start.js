d55145a783f5c14631f6ddaa8b83c5b148f9762d
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-22 21:36:59 +0100

    Write to json file

--- start.js
+++ start.js
@@ -1,6 +1,8 @@
 import request from "request"
 import cheerio from "cheerio"
 import chalk from 'chalk'
+import jsonfile from 'jsonfile'
+
 
 const url = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html"
 const log = console.log;
@@ -9,7 +11,7 @@ request(url, (error, response, body) => {
   const $ = cheerio.load(body)
 
   if (!error) {
-    log(chalk.blue.bgWhite('No error receieved, processing body'))
+    log(chalk.bgGreen.black('No error receieved, processing body'))
     let
       titles = $(".productInfo a").text(),
       urls = $('.productInfo a').map(function(i, el) { return $(el).attr('href').trim() }).toArray(),
@@ -22,7 +24,7 @@ request(url, (error, response, body) => {
         title = title.trim()
         let url =  urls[key]
         if (title > '' && url > '') {
-          log(chalk.green.inverse(`Adding ${title} to the collection`))
+          log(chalk.bgGreen.black(`Adding ${title} to the collection`))
           collection[key] = {
             title: title,
             url: url,
@@ -30,9 +32,15 @@ request(url, (error, response, body) => {
           }
         }
       })
-      log(chalk.yellow.inverse(`Collection complete, listed below:`))
-    console.log(collection)
+      jsonfile.writeFile("data.json", collection.filter((product) => product.title), function (err) {
+        if (err) {
+          return log(chalk.red.inverse(`Failed to write data file: ${err}`))
+        }
+        log(chalk.bgGreen.black(`Data file written successfully`))
+      })
+      log(chalk.bgYellow.black(`Collection complete, listed below:`))
+      log(chalk.bgWhite.black(collection))
   } else {
-    console.log("We’ve encountered an error: " + error)
+    log(chalk.red.inverse(`Failed to write data file ${err}`))
   }
 })
