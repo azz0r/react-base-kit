8faadc7eba3b11f22a151c01c210f43e0081c071
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-22 19:18:32 +0100

    Initial

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import searches from './searches.js'
-import { combineReducers } from 'redux'
-
-const rootReducer = combineReducers({
-  searches,
-});
-
-export default rootReducer;
---
+++ start.js
@@ -0,0 +1,5 @@
+var osmosis = require('osmosis');
+const url = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html"
+
+var instance = new osmosis('http://example.com');
+instance.run();
