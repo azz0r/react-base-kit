b4fb6a1d3c34e7c2ce0665363543be5bf7d04a52
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-22 19:25:22 +0100

    Clean out

--- start.js
+++ start.js
@@ -1,5 +1,23 @@
-var osmosis = require('osmosis');
+const osmosis = require('osmosis');
 const url = "http://hiring-tests.s3-website-eu-west-1.amazonaws.com/2015_Developer_Scrape/5_products.html"
 
-var instance = new osmosis('http://example.com');
-instance.run();
+osmosis
+.get(url)
+.find('h1 + div a')
+.set('location')
+
+.set({
+    'title':        'section > h2',
+    'description':  '#postingbody',
+    'subcategory':  'div.breadbox > span[4]',
+    'date':         'time@datetime',
+    'latitude':     '#map@data-latitude',
+    'longitude':    '#map@data-longitude',
+    'images':       ['img@src']
+})
+.data(function(listing) {
+    // do something with listing data
+})
+.log(console.log)
+.error(console.log)
+.debug(console.log)
