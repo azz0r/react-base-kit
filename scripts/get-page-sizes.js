5939f45e2f2275306b4365ffa68c2a321777634d
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-23 10:20:14 +0100

    Improved logging

--- scripts/get-page-sizes.js
+++ scripts/get-page-sizes.js
@@ -18,12 +18,17 @@ function getPageSizes(collection) {
         }
       )
     )
+    log(
+      chalk.bgYellow.black.bold('Queued fetch for: '),
+      chalk.bgYellow.black(product.url)
+    )
   })
   return Bluebird.all(requests)
     .then((results) => {
       results.forEach((result, key) => {
         collection[key].size = bytesToSize(result.length)
       })
+      log(chalk.bgGreen.bold('Queued product pages complete'))
       return collection
     })
     .catch((err) => {
