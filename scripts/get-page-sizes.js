d09dec55973bd20a381743c439f23f5d41a872cd
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-23 10:27:39 +0100

    Write descriptions

--- scripts/get-page-sizes.js
+++ scripts/get-page-sizes.js
@@ -1,7 +1,8 @@
-import rp from 'request-promise'
-import Bluebird from 'bluebird'
-import chalk from 'chalk'
-import bytesToSize from './bytes-to-size'
+import rp from "request-promise"
+import Bluebird from "bluebird"
+import chalk from "chalk"
+import bytesToSize from "./bytes-to-size"
+import cheerio from "cheerio"
 
 const log = console.log
 
@@ -13,22 +14,24 @@ function getPageSizes(collection) {
     requests.push(
       rp(
         {
-          method: 'GET',
+          method: "GET",
           uri: product.url,
         }
       )
     )
     log(
-      chalk.bgYellow.black.bold('Queued fetch for: '),
+      chalk.bgYellow.black.bold("Queued fetch for: "),
       chalk.bgYellow.black(product.url)
     )
   })
   return Bluebird.all(requests)
     .then((results) => {
       results.forEach((result, key) => {
+        let $ = cheerio.load(result)
+        collection[key].description = $(".productText p").first().text()
         collection[key].size = bytesToSize(result.length)
       })
-      log(chalk.bgGreen.bold('Queued product pages complete'))
+      log(chalk.bgGreen.bold("Queued product pages complete"))
       return collection
     })
     .catch((err) => {
