2c68e3cd005b2b83eead8873c5c0740a5b9a9eb8
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-25 01:41:39 +0100

    remove excess packages from testing

--- src/store/configure-store.js
+++ src/store/configure-store.js
@@ -1,12 +1,10 @@
 import reducers from '../reducers'
 import persistState from 'redux-localstorage'
-import reduxReset from 'redux-reset'
 import { createStore, applyMiddleware, compose } from 'redux'
 
 const storeEnhancer = compose(
   applyMiddleware(),
   persistState(),
-  reduxReset(),
   typeof window === 'object' && typeof window.devToolsExtension !== 'undefined'
     ? window.devToolsExtension()
     : f => f
