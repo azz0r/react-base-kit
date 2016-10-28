9a5d50aa8c0adf239e217719f4ee25014f16c560
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-28 11:07:31 +0100

    Head doesn't need to be a full component

--- src/components/head/head.js
+++ src/components/head/head.js
@@ -1,20 +1,14 @@
 import React from "react"
 import Helmet from "react-helmet"
 
-export default class Head extends React.Component {
-
-  static contextTypes = {
-    constants: React.PropTypes.object.isRequired,
-  }
-
-  displayName = "Head"
-
-  render() {
-    return (
-      <Helmet
-        titleTemplate="%s - Welcome"
-        defaultTitle="Welcome"
-      />
-    )
-  }
+const Head = () => {
+  return (
+    <Helmet
+      titleTemplate="%s - Welcome"
+      defaultTitle="Welcome"
+    />
+  )
 }
+
+
+export default Head
