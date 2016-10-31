08e9b15f2b999a13ca9b409e2835b71248a822ef
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-10-31 23:26:39 +0000

    Make reducers work

--- src/reducers/buckets.js
+++ src/reducers/buckets.js
@@ -1,15 +1,17 @@
 import defaultState from "./buckets.json"
 
 export default (state = defaultState, action) => {
+  let newState = JSON.parse(JSON.stringify(state))
   switch (action.type) {
     case 'MOVE_DROP':
       state.filter((bucket, key) => {
         if (bucket.name === action.bucketName) {
-          state[key].drops.push(
+          newState[key].drops.push(
             {name: action.dropName}
           )
         }
       })
+      return newState
       break
       default:
     break
