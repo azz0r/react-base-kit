40c7eba1e68fdb28f9413a39aa11f452cc3bad9e
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-11-02 19:26:06 +0000

    Reducer update, lets split this into helper functions later

--- src/reducers/buckets.js
+++ src/reducers/buckets.js
@@ -4,17 +4,21 @@ export default (state = defaultState, action) => {
   let newState = JSON.parse(JSON.stringify(state))
   switch (action.type) {
     case 'MOVE_DROP_TO_BUCKET':
+
       let drop = {
         name: action.dropName,
       }
+
+      let isInBucket = (bucket) => {
+        return bucket.drops.filter((obj) => obj.name === action.dropName).length > 0
+      }
+
       state.filter((bucket, key) => {
-        if (
-          (bucket.name === action.bucketName) &&
-          (bucket.drops.filter((obj) => obj.name === drop.name).length === 0)
-        ) {
-          newState[key].drops.push(
-            drop
-          )
+        let isInBucketValue = isInBucket(bucket)
+        if (bucket.name === action.bucketName && !isInBucketValue) {
+          newState[key].drops.push(drop)
+        } else if (isInBucketValue) {
+          newState[key].drops = newState[key].drops.filter((drop) => drop.name !== action.dropName)[0]
         }
       })
       return newState
