2cddb3e5ebaa08695c3c2c2e562fc07a9777bdb8
azz0r <azz0r@azz0rs-MacBook-Pro.local>
2016-11-07 10:31:11 +0000

    No constants when not in use

--- src/components/page/page.js
+++ src/components/page/page.js
@@ -1,7 +1,6 @@
 import React from "react"
 import Head from "../head/head"
 import "../../stylesheets/base"
-import constants from "../../constants"
 
 export default class Page extends React.Component {
   render() {
